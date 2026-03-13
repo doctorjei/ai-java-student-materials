58d3ced4e540c366cb0a07619a55834985c66f8f
SushantKB <tur90290@temple.edu>
2026-03-13 19:31:21 +0900

    Completing Ex 3

--- Assignments/Ex3 - Reversi/Project/Student/Student.Minimax.cs
+++ Assignments/Ex3 - Reversi/Project/Student/Student.Minimax.cs
@@ -12,18 +12,116 @@ public class Minimax : ReversiBehavior
     // found. This method will only be called if there's at least one valid move for player.
     private Move GetBestMove(State board, int depth)
     {
-        //
         // TODO: Perform minimax for each move to determine which is best.
-        //
-        // Return the best move found.
-        return State.NewMove(-1, -1); // Delete this... return what you find!
+        Move best_move = null;
+        State new_state;
+        
+        for (int i = 0; i < board.GRID_SIZE; i++)
+        {
+            for (int j = 0; j < board.GRID_SIZE; j++)
+            {
+                // Consider move at location (row, column) -> (i, j)
+                if (board.IsValidMove(board.Current, i, j))
+                {
+                    new_state = new State(board);
+                    Move move = State.NewMove(i, j);
+                    new_state.MakeMove(move);
+
+                    if (new_state.HasAnyValidMove(-board.Current))
+                    {
+                        new_state.setCurrent(-board.Current);
+                    }
+
+                    if (new_state.IsTerminalState() || (depth == 0))
+                    {
+                        move.Rank = Evaluate(new_state);
+                    }
+                    else
+                    {
+                        move.Rank = GetBestMove(new_state, depth - 1).Rank;
+                    }
+
+                    if (best_move == null)
+                    {
+                        best_move = move;
+                    }
+                    else if (board.Current == 1)
+                    {
+                        if (move.Rank > best_move.Rank)
+                        {
+                            best_move = move;
+                        }
+                    }
+                    else
+                    {
+                        if (move.Rank < best_move.Rank)
+                        {
+                            best_move = move;
+                        }
+                    }
+                }
+            }
+        }
+
+        return best_move; // Return the best move found.
     }
 
     #region Recommended Helper Functions
     private int Evaluate(State board)
     {
         // TODO: Evaluation method for state
-        return 0;
+        int score = 0;
+
+        for (int i = 0; i < board.GRID_SIZE; i++)
+        {
+            for (int j = 0; j < board.GRID_SIZE; j++)
+            {
+                if (i == 0)
+                {
+                    if ((j == 0) || (j == board.GRID_SIZE - 1))
+                    {
+                        score += board[i, j] * 100;
+                    }
+                    else
+                    {
+                        score += board[i, j] * 10;
+                    }
+                }
+                else if (i == board.GRID_SIZE - 1)
+                {
+                    if ((j == 0) || (j == board.GRID_SIZE - 1))
+                    {
+                        score += board[i, j] * 100;
+                    }
+                    else
+                    {
+                        score += board[i, j] * 10;
+                    }
+                }
+                else if ((j == 0) || (j == board.GRID_SIZE - 1))
+                {
+                    score += board[i, j] * 10;
+                }
+                else
+                {
+                    score += board[i, j];
+                }
+            }
+        }
+
+        if (board.IsTerminalState())
+        {
+            if (score > 0)
+            {
+                score += 10000;
+            }
+            else if (score < 0)
+            {
+                score -= 10000;
+            }
+        }
+
+        return score;
     }
     #endregion
 }
